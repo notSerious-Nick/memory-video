482e7c142cd6a6686209fd3b0768010535ae5e4d
nick <123026540+DongnikPark@users.noreply.github.com>
2025-03-31 14:35:24 +0900

    add response

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 4000;
 
 const app = express(); // application(server) has to listent request from client, server turn on all the time
 
-const handleListening = () => console.log("listening!");
+const handleHome = (req, res) => {
+    return res.send("hey"); // if not return with res, the website does unlimited load
+}
+
+app.get("/",handleHome);
+
+const handleListening = () => console.log(`listening! ${PORT}`);
 
 app.listen(PORT, handleListening); // ex) button.addEventListener("click", handleClick)
