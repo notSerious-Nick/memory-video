c07cba57ec8a4ebae8049cbb091b11005f402416
nick <123026540+DongnikPark@users.noreply.github.com>
2025-04-02 23:33:46 +0900

    npm i morgan which is logger

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express"; // node_modules include express file
+import morgan from "morgan";
 
 const PORT = 4000;
+const logger = morgan("dev");
 
 const app = express(); // application(server) has to listent request from client, server turn on all the time
 
@@ -8,6 +10,7 @@ const handleHome = (req, res) => {
     return res.send("hey"); // if not return with res, the website does unlimited load
 }
 
+app.use(logger);
 app.get("/",handleHome);
 
 const handleListening = () => console.log(`listening! ${PORT}`);
